60c19a742250d40f9f36fb1681da983c5810631f
Luis Sanchez <luisalejandross@dynameat.io>
2023-06-26 21:17:15 +0200

    feat adding cache handling in the request

--- src/hooks/usePodcastDetail.js
+++ src/hooks/usePodcastDetail.js
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react';
 import { PodcastDetail } from '../models/podcastDetail';
+import { LocalStorageController } from '../helpers/localStorage';
 
 export const usePodcastDetail = ({ podcastId, setLoading }) => {
   const [podcastDetail, setPodcastDetail] = useState(null);
 
-  const fetchPodcastDetail = async () => {
-    setLoading(true);
+  const validCache = () => {
+    if (LocalStorageController.getPodcastDetail(podcastId)) {
+      return LocalStorageController.isValidTimeStamp(LocalStorageController.getPodcastDetail(podcastId).timestamp);
+    }
+    return false;
+  };
+
+  const apiCall = async () => {
     const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&offset=200&limit=200`)}`);
     if (!response.ok) {
       const message = `An error has occurred while getting the podcast detail information: ${response.status}`;
@@ -14,6 +21,14 @@ export const usePodcastDetail = ({ podcastId, setLoading }) => {
     }
 
     const data = await response.json();
+    LocalStorageController.podcastDetail = { data, id: podcastId };
+    return data;
+  };
+
+  const fetchPodcastDetail = async () => {
+    setLoading(true);
+    const response = validCache() ? LocalStorageController.getPodcastDetail(podcastId).value : await apiCall();
+    const data = await response;
     const content = JSON.parse(data.contents);
     const podcast = content.results.shift();
     const podcastDetailInstance = PodcastDetail.toInstance({ ...podcast, podcastEpisode: content.results });
--- src/hooks/usePodcasts.js
+++ src/hooks/usePodcasts.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from 'react';
 import { Podcast } from '../models/podcast';
+import { LocalStorageController } from '../helpers/localStorage';
 
 export const usePodcasts = (setLoading) => {
   const [podcasts, setPodcasts] = useState([]);
 
-  const fetchPodcasts = async () => {
-    setLoading(true);
-    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')}`);
+  const validCache = () => {
+    if (LocalStorageController.podcastList) {
+      return LocalStorageController.isValidTimeStamp(LocalStorageController.podcastList.timestamp);
+    }
+    return false;
+  };
 
+  const apiCall = async () => {
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')}`);
     if (!response.ok) {
       const message = `An error has occurred while getting the podcast information: ${response.status}`;
       setLoading(false);
       throw new Error(message);
     }
     const data = await response.json();
+    LocalStorageController.podcastList = data;
+    return data;
+  };
+
+  const fetchPodcasts = async () => {
+    setLoading(true);
+    const response = validCache() ? LocalStorageController.podcastList.value : await apiCall();
+    const data = await response;
     const content = JSON.parse(data.contents);
     const listPodcast = Podcast.toInstanceList(content.feed.entry);
     setPodcasts(listPodcast);
